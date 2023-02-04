399196f41c7ed130645f9506a30f7ce8ddfe9929
WafflzPlz <michael.kogler1@gmx.at>
2023-02-04 16:24:11 +0100

    Added test4 test case and sorted it with insertion sort

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,15 @@ public class Main {
     public static void main(String[] args) {
         int[] test1 = {3,6,4,-2,23,1,64,-33,4,800,32,-2000,0,0,432,5,5893,7};
         int[] test2 = {3,6,4,-2,23,1,64,-33,6,78,7965,-22,-346,54,57836,33,24,800,32,-2000,0,0,432,5,5893,7};
-        int[] test3 = new int[100000];
 
+        int[] test3 = new int[100000];
         for (int i = 0; i < test3.length; i++) {
             test3[i] = (int) (Math.random() * 20000);
+        }
+
+        int[] test4 = new int[24653];
+        for (int i = 0; i < test4.length; i++) {
+            test4[i] = (int) (Math.random() * 100000);
         }
 
         SortAlg.insertionSort(test1);
@@ -28,5 +33,9 @@ public class Main {
         SortAlg.selectionSort(test3);
         print(test3);
 
+        System.out.println();
+
+        SortAlg.insertionSort(test4);
+        print(test4);
     }
 }
